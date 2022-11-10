c596754e80994194b1e2805a625e49afe5a3c59e
Kristian-Pacuta <114649894+Kristian-Pacuta@users.noreply.github.com>
2022-11-10 09:52:40 +0100

    idk

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <html>
 <body>
 
-<form action="welcome_get.php" method="get">
+<form action="database.txt" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
+Age: <input type="text" name="age"><br>
+School: <input type="text" name="school"><br>
+Year: <input type="text" name="year"><br>
+Subject: <input type="text" name="subject"><br>
 <input type="submit">
 </form>
 
