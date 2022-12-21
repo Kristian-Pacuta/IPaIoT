3bbf3dc2de0fc7cde0e8476398e380456e4affc4
Jakub-Suda <114649891+Jakub-Suda@users.noreply.github.com>
2022-12-21 22:29:53 +0100

    adjust

--- monitor.php
+++ monitor.php
@@ -1,5 +1,5 @@
 <?php
-header("refresh: 1;");
+header("refresh: 3;");
 $myfile = fopen("sensors.txt", "r") or die("Unable to open file!");
 echo fread($myfile,filesize("sensors.txt"));
 fclose($myfile);
