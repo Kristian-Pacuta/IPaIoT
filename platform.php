8be315e77c868584f9efb54e37546b0c666b0439
Jakub-Suda <114649891+Jakub-Suda@users.noreply.github.com>
2022-12-21 19:09:48 +0100

    Adding &c parameter

--- platform.php
+++ platform.php
@@ -1,11 +1,12 @@
 <?php
-echo '<h1>getParameters page</h1>';
+echo '<h1>Paraneters passing page</h1>';
 
 $sensorValue1 = $_GET["a"];
 $sensorValue2 = $_GET["b"];
+$sensorValue3 = $_GET["c"];
 
 $sensorValuesFile = fopen("sensors.txt","w") or die("Unable to open file!");
-$fileText = $sensorValue1 . "\n" . $sensorValue2;
+$fileText = $sensorValue1 . "\n" . $sensorValue2 . "\n" . $sensorValue3 ;
 fwrite($sensorValuesFile, $fileText);
 fclose($sensorValuesFile);
 
