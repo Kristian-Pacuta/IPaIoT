7fad2ba344f04d4049e2b3d3f816540998205b57
Jakub-Suda <114649891+Jakub-Suda@users.noreply.github.com>
2022-12-21 18:31:39 +0100

    nameEdit

--- platform.php
+++ platform.php
@@ -4,7 +4,7 @@ echo '<h1>getParameters page</h1>';
 $sensorValue1 = $_GET["a"];
 $sensorValue2 = $_GET["b"];
 
-$sensorValuesFile = fopen("values.txt","w") or die("Unable to open file!");
+$sensorValuesFile = fopen("sensors.txt","w") or die("Unable to open file!");
 $fileText = $sensorValue1 . "\n" $sensorValue2;
 fwrite($sensorValuesFile, $fileText);
 fclose($sensorValuesFile);
