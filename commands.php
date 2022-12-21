d22bd9a004fe44e526bd8c9a05c36a7172661d4e
Jakub-Suda <114649891+Jakub-Suda@users.noreply.github.com>
2022-12-22 00:03:41 +0100

    rep

--- commands.php
+++ commands.php
@@ -9,12 +9,11 @@
 <form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
 <h3>Choose type of movement: <br> <br>
-<ul>
- <li> <input type="radio" id="1" name="movement" value="0" checked="checked">
-  <label for="1"> Turning follow</label></li><br><br>
-  <li><input type="radio" id="2" name="movement" value="1">
-  <label for="2"> Crab walk follow</label></li></h3><br><br>
-  </ul>
+  <input type="radio" id="1" name="movement" value="0" checked="checked">
+  <label for="1"> Turning follow</label><br><br>
+  <input type="radio" id="2" name="movement" value="1">
+  <label for="2"> Crab walk follow</label></h3><br><br>
+
 <input type="submit">
 </form>
 
