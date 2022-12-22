f975e171550afd0941325a5b1779ab4813cb1d1e
Jakub-Suda <114649891+Jakub-Suda@users.noreply.github.com>
2022-12-22 02:44:58 +0100

    adjust

--- monitor.php
+++ monitor.php
@@ -1,15 +1,15 @@
 <?php
 header("refresh: 3;");
 
-echo "<h1>" . "Sensor monitor: " . "</h1>" . "<br>";
+echo "<h3>" . "Sensor monitor: " . "</h3>" . "<br>";
 $fp = fopen("sensors.txt", "r") or die("Unable to open file!");
 $x=fgets($fp);
 $y=fgets($fp);
 $distance=fgets($fp);
 
-    echo "Gyroscope X-axis acceleration: " . $x . " m/s2" . "<br>";
-    echo "Gyroscope Y-axis acceleration: " . $y . " m/s2" . "<br>";
-    echo "Estimated sonar distance: " . $distance . " cm" . "<br>";
+    echo "Gyroscope X-axis acceleration: " . $x . " m/s2" . "<br>" . "<br>";
+    echo "Gyroscope Y-axis acceleration: " . $y . " m/s2" . "<br>" . "<br>";
+    echo "Estimated sonar distance: " . $distance . " cm" . "<br>" . "<br>";
     fclose($fp);
 
 
