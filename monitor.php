83a6654cfc986c682646a2bbd8806dc3455e5336
Jakub-Suda <114649891+Jakub-Suda@users.noreply.github.com>
2022-12-22 02:07:35 +0100

    rep2

--- monitor.php
+++ monitor.php
@@ -9,17 +9,17 @@ header("refresh: 3;");
 
 
 
-//$myfile = fopen("sensors.txt", "r") or die("Unable to open file!");
-//echo fread($myfile,filesize("sensors.txt"));
-//fclose($myfile);
-
-
 $myfile = fopen("sensors.txt", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("sensors.txt"));
+fclose($myfile);
+
+
+/*$myfile = fopen("sensors.txt", "r") or die("Unable to open file!");
     while (($line = fgets($myfile)) !== false) {
         // process the line read.
         $word_arr = explode(" ", $line); //return word array
         foreach($word_arr as $word){
             echo $word; // required output
         }
-    fclose($myfile);
+    fclose($myfile);*/
 ?>
