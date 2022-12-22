6c55583b962e3fc3e3c64102cac4af7a6069ea90
Jakub-Suda <114649891+Jakub-Suda@users.noreply.github.com>
2022-12-22 02:18:35 +0100

    experiment

--- monitor.php
+++ monitor.php
@@ -1,25 +1,20 @@
 <?php
 header("refresh: 3;");
 
-
-
-
-
-
-
-
-
-$myfile = fopen("sensors.txt", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("sensors.txt"));
-fclose($myfile);
+$fp = fopen("sensors.txt", "r");
+if ($fp) {
+    while (($buffer = fgets($fp, 4096)) !== false) {
+        echo $buffer;
+    }
+    if (!feof($fp)) {
+        echo "Error: unexpected fgets() fail\n";
+    }
+    fclose($fp);
+}
 
 
 /*$myfile = fopen("sensors.txt", "r") or die("Unable to open file!");
-    while (($line = fgets($myfile)) !== false) {
-        // process the line read.
-        $word_arr = explode(" ", $line); //return word array
-        foreach($word_arr as $word){
-            echo $word; // required output
-        }
-    fclose($myfile);*/
+echo fread($myfile,filesize("sensors.txt"));
+fclose($myfile);*/
+
 ?>
