ada625f6ee24e5bf409372af4f5b3bba023fec63
Kristian-Pacuta <114649894+Kristian-Pacuta@users.noreply.github.com>
2023-01-12 09:44:00 +0100

    dd

--- skuska.php
+++ skuska.php
@@ -9,31 +9,31 @@
     <label for="0">0</label>
     <input type="radio" id="0" name="number1" value="0">
     
-    <label for="1">1</label>
+    <label for=",1">1</label>
     <input type="radio" id="1" name="number1" value="1">
     
-    <label for="2">2</label>
+    <label for=",2">2</label>
     <input type="radio" id="2" name="number1" value="2">
     
-    <label for="3">3</label>
+    <label for=",3">3</label>
     <input type="radio" id="3" name="number1" value="3">
     
-    <label for="4">4</label>
+    <label for=",4">4</label>
     <input type="radio" id="4" name="number1" value="4">
     
-    <label for="5">5</label>
+    <label for=",5">5</label>
     <input type="radio" id="5" name="number1" value="5">
     
-    <label for="6">6</label>
+    <label for=",6">6</label>
     <input type="radio" id="6" name="number1" value="6">
     
-    <label for="7">7</label>
+    <label for=",7">7</label>
     <input type="radio" id="7" name="number1" value="7">
     
-    <label for="8">8</label>
+    <label for=",8">8</label>
     <input type="radio" id="8" name="number1" value="8">
     
-    <label for="9">9</label>
+    <label for=",9">9</label>
     <input type="radio" id="9" name="number1" value="9">
 </h3><br>
 
