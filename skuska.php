434fe06aaaf33230a3920da3dc09ae1a2b73950f
Kristian-Pacuta <114649894+Kristian-Pacuta@users.noreply.github.com>
2023-01-12 09:07:57 +0100

    first try

--- skuska.php
+++ skuska.php
@@ -1,1 +1,54 @@
-#,,,,
+<html>
+<head>
+  <link rel="stylesheet" href="screenStyle.css">
+</head>
+<body>
+
+<form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<h3>Number:
+    <label for="0">0</label>
+    <input type="radio" id="0" name="year" value="0">
+    
+    <label for="1">1</label>
+    <input type="radio" id="1" name="year" value="1">
+    
+    <label for="2">2</label>
+    <input type="radio" id="2" name="year" value="2">
+    
+    <label for="3">3</label>
+    <input type="radio" id="3" name="year" value="3">
+    
+    <label for="4">4</label>
+    <input type="radio" id="4" name="year" value="4">
+    
+    <label for="5">5</label>
+    <input type="radio" id="5" name="year" value="5">
+    
+    <label for="6">6</label>
+    <input type="radio" id="6" name="year" value="6">
+    
+    <label for="7">7</label>
+    <input type="radio" id="7" name="year" value="7">
+    
+    <label for="8">8</label>
+    <input type="radio" id="8" name="year" value="8">
+    
+    <label for="9">9</label>
+    <input type="radio" id="9" name="year" value="9">
+  
+</h3><br>
+<input type="submit">
+</form>
+
+<?php
+    
+    $year = $_GET["number"];
+
+    $file1 = fopen("skuska.txt","w") or die("Unable to open file!");
+    $text1 = "number: " . $number;    
+    fwrite($file1, $text1);
+    fclose($file1);
+?>
+
+</body>
+</html> 
